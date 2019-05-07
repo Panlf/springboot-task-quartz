bd9d50a6060cbda7973fdec5c6a1c8ea7458bd2d
Panlf <liangfeng_pan@163.com>
2019-05-07 16:18:30 +0800

    first commit

---
+++ src/main/java/com/plf/task/quartz/SpringbootTaskQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.plf.task.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTaskQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTaskQuartzApplication.class, args);
+	}
+
+}
